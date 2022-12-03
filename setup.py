f7873b2eddd90b8d175d1e836ea27b4ace6c67a6
Ronen Margolin <roni948576@gmail.com>
2022-12-03 17:47:11 +0200

    added github link

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ from setuptools import setup
 setup(name="ronen-server",
     version=0.1,
     description="a calculator server",
+    url="https://github.com/Ronemisimus/ronen-server",
     author="Ronen Margolin",
     license="MIT",
     packages=["ronen-server"],
