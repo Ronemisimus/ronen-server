e3b725ef7aa4e2cafc70bc7ce557bca7f889be8f
Ronen Margolin <roni948576@gmail.com>
2022-12-04 21:38:31 +0200

    working shiv reorder

---
+++ hello.py
@@ -0,0 +1,12 @@
+from server import create_app
+from waitress import serve
+
+
+def main():
+    app = create_app()
+    port = 8496
+    print('serving app on http://localhost:8496')
+    serve(app,port=port)
+
+if __name__ == "__main__":
+    main()
---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name="ronen-server",
+    version="0.0.1",
+    description="Greet the world.",
+    py_modules=["hello","server","calculate","stack","global_strings"],
+    install_requires=['flask','waitress'],
+    packages=['ronen-server'],
+    include_package_data=True,
+    zip_safe=False,
+    entry_points={
+        "console_scripts": ["hello=hello:main"],
+    },
+)
