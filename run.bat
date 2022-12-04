1e4721beb54f372ed5749ae66cc17ba6c492013c
Ronen Margolin <roni948576@gmail.com>
2022-12-04 13:02:51 +0200

    better sh file creates a single package dir

--- run.bat
+++ run.bat
@@ -1,2 +1,4 @@
-pip install --root=. .
+setlocal
+SET PATH=%~dp0\site-packages;%PATH%
+pip install --root=site-packages .
 python -m ronen-server
--- run.sh
+++ run.sh
@@ -1,2 +1,6 @@
-pip install --root=. .
+TEMPPATH=$PATH
+PATH="$(pwd)/site-packages;$PATH" 
+printf "%s\r\n\n" $PATH
+pip install --root=site-packages .
 python -m ronen-server
+PATH=$TEMPPATH
